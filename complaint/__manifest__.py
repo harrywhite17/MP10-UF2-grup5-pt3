ef07aa474b3e008a75ebad8b1fca6a3bb4ca6649
gerardmoreno123 <gerardmoreno@iesebre.com>
2025-03-13 17:51:33 +0100

    Sale order view

--- complaint/__manifest__.py
+++ complaint/__manifest__.py
@@ -7,7 +7,8 @@
     'security/ir.model.access.csv',
     'views/complaint_views.xml',
     'views/complaint_reason_views.xml',
-    'views/complaint_message_views.xml'
+    'views/complaint_message_views.xml',
+    'views/sale_order_views.xml',
  ],
  "installable": True,
  "license": "LGPL-3",
--- complaint/models/sale_order.py
+++ complaint/models/sale_order.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    complaint_ids = fields.One2many('complaint.complaint', 'order_id', string="Reclamacions")
+    complaint_ids = fields.One2many('complaint.complaint', 'order_id', string="Reclamacions", readonly=False)

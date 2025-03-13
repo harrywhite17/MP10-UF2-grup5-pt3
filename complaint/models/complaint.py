a67f0cb8af9fa8cdaae2581a755013558880d8d1
harrywhite17 <harrywhite@iesebre.com>
2025-03-13 16:12:07 +0100

    Fixed Factures associades Enviaments Associats

--- complaint/models/complaint.py
+++ complaint/models/complaint.py
@@ -5,7 +5,7 @@ from odoo import exceptions
 class Complaint(models.Model):
     _name = 'complaint.complaint'
     _description = 'Client Complaint'
-    
+
     name = fields.Char(string="Assumpte", required=True)
     description = fields.Text(string="Descripció inicial", required=True)
     state = fields.Selection([
@@ -14,19 +14,19 @@ class Complaint(models.Model):
         ('closed', 'Tancada'),
         ('cancelled', 'Cancel·lada')
     ], string="Estat", default="new", required=True)
-    
+
     order_id = fields.Many2one('sale.order', string="Comanda Associada", required=True)
     partner_id = fields.Many2one('res.partner', string="Client", required=True)
     user_id = fields.Many2one('res.users', string="Usuari Responsable", default=lambda self: self.env.user)
-    
+
     create_date = fields.Datetime(string="Data de Creació", readonly=True, default=fields.Datetime.now)
     write_date = fields.Datetime(string="Data de Modificació", readonly=True)
     close_date = fields.Datetime(string="Data de Tancament", readonly=True)
-    
+
     message_ids = fields.One2many('complaint.message', 'complaint_id', string="Missatges")
     invoice_count = fields.Integer(compute='_compute_invoice_count', string='Factures associades')
     delivery_count = fields.Integer(string="Enviaments Associats", compute="_compute_delivery_count")
-    
+
     resolution = fields.Text(string="Descripció de la Resolució Final")
     reason_id = fields.Many2one('complaint.reason', string="Motiu de Tancament/Cancel·lació")
 
